1bdf6ea592e9f64841fdd6439c4e8a48b8d4a802
Pascal Horton <horton.pascal@gmail.com>
2020-10-29 15:15:13 +0100

    Updating code coverage CMake file.

---
+++ tests/src/HydroUnitTest.cpp
@@ -0,0 +1,18 @@
+
+#include <gtest/gtest.h>
+
+#include "HydroUnit.h"
+
+TEST(HydroUnit, BuildsCorrectly) {
+    HydroUnit unit(100, HydroUnit::Lumped);
+
+    EXPECT_EQ(100, unit.GetArea());
+    EXPECT_EQ(HydroUnit::Lumped, unit.GetType());
+}
+
+TEST(HydroUnit, HasCorrectId) {
+    HydroUnit unit;
+    unit.SetId(9);
+
+    EXPECT_EQ(9, unit.GetId());
+}

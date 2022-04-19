b53d4a48f3fc1086911d10dab2ddb4ab67f3258a
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-04-19 17:01:50 +0200

    Basic model builds correctly.

--- tests/src/ModelHydroTest.cpp
+++ tests/src/ModelHydroTest.cpp
@@ -2,14 +2,26 @@
 
 #include "helpers.h"
 #include "ModelHydro.h"
+#include "SolverRungeKuttaMethod.h"
+#include "StorageLinear.h"
+#include "FluxDirect.h"
 
 TEST(ModelHydro, BuildsCorrectly) {
+    SolverRungeKuttaMethod solver;
+    Processor processor(&solver);
     TimeMachine timer = GenerateTimeMachineDaily();
-    auto* subBasin = new SubBasin();
-    auto* unit = new HydroUnit();
-    subBasin->AddHydroUnit(unit);
-    ModelHydro model(subBasin, timer);
+    SubBasin subBasin;
+    HydroUnit unit;
+
+    StorageLinear storage(&unit);
+    FluxDirect inFlux, outFlux;
+    storage.AttachFluxIn(&inFlux);
+    storage.AttachFluxOut(&outFlux);
+
+    unit.SetInputFlux(&inFlux);
+
+    subBasin.AddHydroUnit(&unit);
+    ModelHydro model(&processor, &subBasin, &timer);
 
     EXPECT_TRUE(model.IsOk());
 }
-

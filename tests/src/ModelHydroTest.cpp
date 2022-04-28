b0ed3543949c89b9ccd4c2e5382f9aa14c263593
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-04-28 18:40:35 +0200

    Adding test fixture.

--- tests/src/ModelHydroTest.cpp
+++ tests/src/ModelHydroTest.cpp
@@ -2,42 +2,41 @@
 
 #include "ModelHydro.h"
 #include "ParameterSet.h"
-#include "helpers.h"
 
-TEST(ModelHydro, BuildsCorrectly) {
-    ParameterSet parameterSet;
-    parameterSet.SetSolver("ExplicitEuler");
-    parameterSet.SetTimer("2020-01-01", "2020-01-10", 1, "Day");
-    parameterSet.AddBrick("linear-storage", "StorageLinear");
-    parameterSet.AddParameterToCurrentBrick("responseFactor", 0.1);
-    parameterSet.AddForcingToCurrentBrick("Precipitation");
-    parameterSet.AddOutputToCurrentBrick("outlet");
+class ModelHydroSingleLinearStorage : public ::testing::Test {
+  protected:
+    ParameterSet m_parameterSet;
 
+    virtual void SetUp() {
+        m_parameterSet.SetSolver("ExplicitEuler");
+        m_parameterSet.SetTimer("2020-01-01", "2020-01-10", 1, "Day");
+        m_parameterSet.AddBrick("linear-storage", "StorageLinear");
+        m_parameterSet.AddParameterToCurrentBrick("responseFactor", 0.1);
+        m_parameterSet.AddForcingToCurrentBrick("Precipitation");
+        m_parameterSet.AddOutputToCurrentBrick("outlet");
+    }
+    virtual void TearDown() {
+    }
+};
+
+TEST_F(ModelHydroSingleLinearStorage, BuildsCorrectly) {
     SubBasin subBasin;
     HydroUnit unit;
     subBasin.AddHydroUnit(&unit);
 
-    ModelHydro* model = ModelHydro::Factory(parameterSet, &subBasin);
+    ModelHydro* model = ModelHydro::Factory(m_parameterSet, &subBasin);
 
     EXPECT_TRUE(model->IsOk());
 
     wxDELETE(model);
 }
 
-TEST(ModelHydro, RunsCorrectly) {
-    ParameterSet parameterSet;
-    parameterSet.SetSolver("ExplicitEuler");
-    parameterSet.SetTimer("2020-01-01", "2020-01-10", 1, "Day");
-    parameterSet.AddBrick("linear-storage", "StorageLinear");
-    parameterSet.AddParameterToCurrentBrick("responseFactor", 0.1);
-    parameterSet.AddForcingToCurrentBrick("Precipitation");
-    parameterSet.AddOutputToCurrentBrick("outlet");
-
+TEST_F(ModelHydroSingleLinearStorage, RunsCorrectly) {
     SubBasin subBasin;
     HydroUnit unit;
     subBasin.AddHydroUnit(&unit);
 
-    ModelHydro* model = ModelHydro::Factory(parameterSet, &subBasin);
+    ModelHydro* model = ModelHydro::Factory(m_parameterSet, &subBasin);
 
     EXPECT_FALSE(model->Run());
 

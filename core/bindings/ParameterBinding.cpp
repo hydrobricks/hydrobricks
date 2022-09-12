9a81d1fed81b637f8f0bf3596159fd419cc928b8
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-09-12 21:54:08 +0200

    Pybind11 example also building in debug mode.

--- core/bindings/ParameterBinding.cpp
+++ core/bindings/ParameterBinding.cpp
@@ -1,13 +1,14 @@
 #include <pybind11/pybind11.h>
 
-namespace py = pybind11;
 
-int xadd(int i, int j) {
+int add(int i, int j) {
     return i + j;
 }
+
+namespace py = pybind11;
 
 PYBIND11_MODULE(hydrobricks, m) {
 m.doc() = "pybind11 example plugin"; // optional module docstring
 
-m.def("add", &xadd, "A function that adds two numbers");
+m.def("add", &add, "A function that adds two numbers");
 }

1d1c73df132dec1d322b12cc6984db1596ed3ff3
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-09-28 17:30:05 +0200

    Working on the Python binding.

--- core/bindings/Binding.cpp
+++ core/bindings/Binding.cpp
@@ -2,15 +2,25 @@
 
 #include "Includes.h"
 #include "Parameter.h"
+#include "ParameterVariable.h"
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 PYBIND11_MODULE(hydrobricks, m) {
     m.doc() = "hydrobricks Python interface";
 
     py::class_<Parameter>(m, "Parameter")
-        .def(py::init<const std::string &>())
-        .def("set_name", &Parameter::SetName)
-        .def("get_name", &Parameter::GetName);
+        .def(py::init<const std::string &, float>())
+        .def_property("name", &Parameter::GetName, &Parameter::SetName)
+        .def("__repr__",
+             [](const Parameter &a) {
+                 return "<hydrobricks.Parameter named '" + a.GetName() + "'>";
+             }
+        );
 
+    py::class_<ParameterVariableYearly, Parameter>(m, "ParameterVariableYearly")
+        .def(py::init<const std::string &, float>())
+        .def("set_values", &ParameterVariableYearly::SetValues, "Set the parameter values",
+            "year_start"_a, "year_end"_a, "values"_a);
 }
--- python/tests/test_binding.py
+++ python/tests/test_binding.py
@@ -3,4 +3,4 @@ import hydrobricks as hb
 
 def test_main():
     param = hb.Parameter("super name")
-    assert param.get_name() == "super name"
+    assert param.name == "super name"

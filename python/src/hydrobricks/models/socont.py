613cd33b7d1b8c4c7b3ee21b2775ed4ab07940d5
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-10-05 16:48:56 +0200

    Checking model initialization.

--- python/src/hydrobricks/models/socont.py
+++ python/src/hydrobricks/models/socont.py
@@ -15,9 +15,13 @@ class Socont(Model):
         self._validate_kwargs(kwargs)
         self._set_options(kwargs)
 
-        self.structure.generate_socont_structure(
-            self.surface_types, self.surface_names,
-            self.soil_storage_nb, self.surface_runoff)
+        try:
+            if not self.structure.generate_socont_structure(
+                    self.surface_types, self.surface_names,
+                    self.soil_storage_nb, self.surface_runoff):
+                raise Exception('Socont model initialization failed.')
+        except RuntimeError as err:
+            raise Exception(f'Socont model initialization raised an exception: {err}')
 
     def _set_options(self, kwargs):
         super()._set_options(kwargs)

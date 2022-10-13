d7e63c7c9fba51503a31e729168d836edf323e5a
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-10-13 23:29:40 +0200

    Incrementing version.

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ long_description = (this_directory / "python" / "README.md").read_text()
 # Setup
 setup(
     name="hydrobricks",
-    version="0.0.5",
+    version="0.0.6",
     author="Pascal Horton",
     author_email="pascal.horton@giub.unibe",
     description="A modular hydrological modelling framework",

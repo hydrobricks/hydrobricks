afa2e3f6e744502a013290e0b49fae41a291b436
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-12-02 00:35:32 +0100

    Fixing preprocessing module import.

---
+++ python/src/hydrobricks/preprocessing/__init__.py
@@ -0,0 +1,6 @@
+from _hydrobricks import init
+
+from .catchment import Catchment
+
+init()
+__all__ = ('Catchment',)
--- python/tests/test_preprocessing_catchment.py
+++ python/tests/test_preprocessing_catchment.py
@@ -2,7 +2,7 @@ import os.path
 from pathlib import Path
 
 import hydrobricks as hb
-from hydrobricks.preprocessing.catchment import Catchment
+import hydrobricks.preprocessing as preprocessing
 
 TEST_FILES_DIR = Path(
     os.path.dirname(os.path.realpath(__file__)),
@@ -19,20 +19,20 @@ def has_required_packages() -> bool:
 def test_shapefile_parsing():
     if not has_required_packages():
         return
-    Catchment(CATCHMENT_OUTLINE)
+    preprocessing.Catchment(CATCHMENT_OUTLINE)
 
 
 def test_dem_extraction():
     if not has_required_packages():
         return
-    catchment = Catchment(CATCHMENT_OUTLINE)
+    catchment = preprocessing.Catchment(CATCHMENT_OUTLINE)
     assert catchment.extract_dem(CATCHMENT_DEM)
 
 
 def test_elevation_bands_isohypses():
     if not has_required_packages():
         return
-    catchment = Catchment(CATCHMENT_OUTLINE)
+    catchment = preprocessing.Catchment(CATCHMENT_OUTLINE)
     catchment.extract_dem(CATCHMENT_DEM)
     bands = catchment.get_elevation_bands(method='isohypse', distance=50)
     # bands.to_csv('elevation_bands.csv')
@@ -42,7 +42,7 @@ def test_elevation_bands_isohypses():
 def test_elevation_bands_quantiles():
     if not has_required_packages():
         return
-    catchment = Catchment(CATCHMENT_OUTLINE)
+    catchment = preprocessing.Catchment(CATCHMENT_OUTLINE)
     catchment.extract_dem(CATCHMENT_DEM)
     bands = catchment.get_elevation_bands(method='quantiles', number=25)
     assert 74430000 < bands['area'].sum() < 74450000
@@ -51,7 +51,7 @@ def test_elevation_bands_quantiles():
 def test_get_mean_elevation():
     if not has_required_packages():
         return
-    catchment = Catchment(CATCHMENT_OUTLINE)
+    catchment = preprocessing.Catchment(CATCHMENT_OUTLINE)
     catchment.extract_dem(CATCHMENT_DEM)
     mean_elevation = catchment.get_mean_elevation()
     assert 1200 < mean_elevation < 1300

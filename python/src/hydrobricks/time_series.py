b51c9d7d38b8f878e3d1f168a9add78af81b25a4
Pascal Horton <pascal.horton@unibe.ch>
2023-08-21 10:47:30 +0200

    Removing the use of the csv format for the spatialized data.
    Using the netCDF file is safer.

--- python/src/hydrobricks/time_series.py
+++ python/src/hydrobricks/time_series.py
@@ -1,13 +1,13 @@
-from datetime import datetime
-
-import hydrobricks as hb
 import numpy as np
 import pandas as pd
+
+import hydrobricks as hb
 from hydrobricks import utils
 
 
 class TimeSeries:
     """Class for generic time series data"""
+
     def __init__(self):
         self.time = []
         self.data = []
@@ -19,6 +19,7 @@ class TimeSeries:
 
 class TimeSeries1D(TimeSeries):
     """Class for generic 1D time series data"""
+
     def __init__(self):
         super().__init__()
 
@@ -51,42 +52,18 @@ class TimeSeries1D(TimeSeries):
 
 class TimeSeries2D(TimeSeries):
     """Class for generic 2D time series data"""
+
     def __init__(self):
         super().__init__()
 
-    def load_from_csv(self, column, path, time_format):
-        """
-        Read already spatialized time series data from csv file.
-        Useful for data that needs a long time to be processed with initialize_from_netcdf_HR()
-        or initialize_from_netcdf() and can be stored as csv file.
-
-        Parameters
-        ----------
-        column : str
-            Name of the data to be loaded.
-        path : str|Path
-            Path to the csv file containing hydro units data.
-        time_format : str
-            Format of the time
-        """
-        data = np.loadtxt(path, delimiter=',')
-
-        file_content = pd.read_csv(
-            path, parse_dates=['time'],
-            date_format=time_format)
-
-        nb_days = len(data)
-        assert len(file_content['# time']) == nb_days
-
-        self.time = file_content['# time'] # time2 #file_content[column_time]
-        self.data_name.append(column)
-        self.data.append(data)
-
-    def initialize_from_netcdf(self, path, varname, elev_mask_path, elevation_thrs, column_time, time_format, content):
+    def initialize_from_netcdf(self, path, varname, elev_mask_path, elevation_thrs,
+                               column_time, time_format, content):
         """
         Initialize time series data from a netcdf file.
-        Uses a DEM in netcdf format interpolated to the meteorological data resolution as input.
-        Does not load the data in memory, as storing all data would be too RAM consuming.
+        Uses a DEM in netcdf format interpolated to the meteorological
+        data resolution as input.
+        Does not load the data in memory, as storing all data would be
+        too RAM consuming.
         Returns the start and end date of the dataset.
 
         Parameters
@@ -98,7 +75,8 @@ class TimeSeries2D(TimeSeries):
         elev_mask_path : str|Path
             Path to the DEM in netcdf format.
         elevation_thrs : list of float
-            List of the threshold elevations defining the beginning and end of the elevation bands (hydrological units).
+            List of the threshold elevations defining the beginning and end of the
+            elevation bands (hydrological units).
         column_time : str
             Column name containing the time.
         time_format : str
@@ -125,13 +103,19 @@ class TimeSeries2D(TimeSeries):
         assert len(elevation_thrs) == band_nb + 1
         data = np.zeros((band_nb, times))
         for i, elev_min in enumerate(elevation_thrs[:-1]):
-            elev_max = elevation_thrs[i+1]
-            topo_mask = hb.xr.where((topo > elev_min) & (topo < elev_max), 1, 0) # Elevation band set to 1, rest to 0.
-            topo_mask_3d = hb.xr.concat([topo_mask] * times, dim=column_time) # Multiply by the depth of the meteorological time serie.
-            topo_mask_3d = topo_mask_3d.assign_coords(time=var.variables[column_time][:]) # Assign the times to these sheets.
-
-            val = hb.xr.where(topo_mask_3d, var, np.nan) # Mask the meteorological data with the current elevation band.
-            data[i] = hb.xr.DataArray.mean(val, dim=["x", "y"], skipna=True)[varname].to_numpy() # Mean the meteorological data in the band.
+            elev_max = elevation_thrs[i + 1]
+            topo_mask = hb.xr.where((topo > elev_min) & (topo < elev_max), 1,
+                                    0)  # Elevation band set to 1, rest to 0.
+            # Multiply by the depth of the meteorological time serie.
+            topo_mask_3d = hb.xr.concat([topo_mask] * times,
+                                        dim=column_time)
+            topo_mask_3d = topo_mask_3d.assign_coords(
+                time=var.variables[column_time][:])  # Assign the times to these sheets.
+            # Mask the meteorological data with the current elevation band.
+            val = hb.xr.where(topo_mask_3d, var,
+                              np.nan)
+            data[i] = hb.xr.DataArray.mean(val, dim=["x", "y"], skipna=True)[
+                varname].to_numpy()  # Mean the meteorological data in the band.
             # We do not yet interpolate the holes.
 
         self.time = column
@@ -141,11 +125,15 @@ class TimeSeries2D(TimeSeries):
 
         return var.variables[column_time][0], var.variables[column_time][-1]
 
-    def initialize_from_netcdf_HR(self, path, varname, elev_mask_path, elevation_thrs, tif_CRS, nc_CRS, column_time, time_format, content):
+    def initialize_from_netcdf_HR(self, path, varname, elev_mask_path, elevation_thrs,
+                                  tif_CRS, nc_CRS, column_time, time_format, content):
         """
-        Initialize time series data from a netcdf file. Uses a DEM in tif format at any resolution.
-        The meteorological data is resampled to the DEM resolution using bilinear interpolation.
-        Does not load the data in memory, as storing all data would be too RAM consuming.
+        Initialize time series data from a netcdf file. Uses a DEM in tif format
+        at any resolution.
+        The meteorological data is resampled to the DEM resolution using bilinear
+        interpolation.
+        Does not load the data in memory, as storing all data would be too RAM
+        consuming.
         Returns the start and end date of the dataset.
 
         Parameters
@@ -157,7 +145,8 @@ class TimeSeries2D(TimeSeries):
         elev_mask_path : str|Path
             Path to the DEM in tif format.
         elevation_thrs : list of float
-            List of the threshold elevations defining the beginning and end of the elevation bands (hydrological units).
+            List of the threshold elevations defining the beginning and end of
+            the elevation bands (hydrological units).
         tif_CRS : str
             CRS of the input tif file written in 'EPSG:4326' format.
         nc_CRS : str
@@ -196,17 +185,22 @@ class TimeSeries2D(TimeSeries):
         assert len(elevation_thrs) == band_nb + 1
         all_topo_masks = []
         for i, elev_min in enumerate(elevation_thrs[:-1]):
-            elev_max = elevation_thrs[i+1]
-            topo_mask = hb.xr.where((topo > elev_min) & (topo < elev_max), 1, 0) # Elevation band set to 1, rest to 0.
+            elev_max = elevation_thrs[i + 1]
+            topo_mask = hb.xr.where((topo > elev_min) & (topo < elev_max), 1,
+                                    0)  # Elevation band set to 1, rest to 0.
             all_topo_masks.append(topo_mask)
 
         data = np.zeros((band_nb, times))
         for j, time in enumerate(var.variables[column_time][:]):
             print(time)
-            var_uniq = var[varname][j].rio.reproject_match(topo, Resampling=hb.rasterio.enums.Resampling.bilinear)
-            for i, elev_min in enumerate(elevation_thrs[:-1]):
-                val = hb.xr.where(all_topo_masks[i], var_uniq, np.nan).to_array() # Mask the meteorological data with the current elevation band.
-                data[i][j] = np.nanmean(val) # Mean the meteorological data in the band.
+            var_uniq = var[varname][j].rio.reproject_match(
+                topo, Resampling=hb.rasterio.enums.Resampling.bilinear)
+            for i, _ in enumerate(elevation_thrs[:-1]):
+                # Mask the meteorological data with the current elevation band.
+                val = hb.xr.where(all_topo_masks[i], var_uniq,
+                                  np.nan).to_array()
+                data[i][j] = np.nanmean(
+                    val)  # Mean the meteorological data in the band.
                 # We do not yet interpolate the holes.
 
         self.time = column

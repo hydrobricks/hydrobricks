ee2d96244fac52a34cb592121d1cdc862895f602
Pascal Horton <pascal.horton@unibe.ch>
2023-08-23 11:17:57 +0200

    Fixing data container size.

--- python/src/hydrobricks/time_series.py
+++ python/src/hydrobricks/time_series.py
@@ -165,7 +165,7 @@ class TimeSeries2D(TimeSeries):
             unit_id_masks.append(unit_id_mask)
 
         # Initialize data array
-        data = np.zeros((unit_ids_nb, len(self.time)))
+        data = np.zeros((len(self.time), unit_ids_nb))
         self.data.append(data)
 
         # Drop other variables

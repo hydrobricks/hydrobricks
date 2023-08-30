3bdeeeed606180e612fcbe79ca162670aee2767b
Pascal Horton <pascal.horton@unibe.ch>
2023-08-30 10:11:08 +0200

    Adding an extraction of the forcing using a weighted approach.

--- python/src/hydrobricks/time_series.py
+++ python/src/hydrobricks/time_series.py
@@ -186,15 +186,61 @@ class TimeSeries2D(TimeSeries):
         # Time the computation
         start_time = time.time()
 
-        # Create a ThreadPoolExecutor with a specified number of threads
         num_threads = os.cpu_count()
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")  # Ignoring a warning from pyproj
+
+        method = 2
+        if method == 1:
+            # Create a ThreadPoolExecutor with a specified number of threads
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")  # Ignoring a warning from pyproj
+                with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                    # Submit the tasks for each time step to the executor
+                    futures = [executor.submit(self._extract_time_step_data_reproject,
+                                               data_var, unit_id_masks, unit_ids,
+                                               unit_ids_nb, t)
+                               for t in range(len(self.time))]
+
+                    # Wait for all tasks to complete
+                    concurrent.futures.wait(futures)
+
+        elif method == 2:
+            # Create a xarray variable containing the data cell indices
+            data_idx = data_var[0].copy()
+            data_idx.values = np.arange(data_idx.size).reshape(data_idx.shape)
+            data_idx = data_idx.astype(float)
+
+            # Reproject the data cell indices to the hydro unit raster
+            data_idx_reproj = data_idx.rio.reproject_match(
+                unit_ids, Resampling=hb.rasterio.enums.Resampling.nearest)
+
+            # Create the masks (with the original data shape) for each unit with the
+            # weights to apply to the gridded data contributing to the unit
+            unit_weights = []
+            for u in range(unit_ids_nb):
+                # Get the data indices contributing to the unit
+                mask_unit_id = hb.xr.where(unit_id_masks[u], data_idx_reproj, -1)
+                mask_unit_id = mask_unit_id.to_numpy().astype(int)
+                # Get unique values and their counts
+                data_idx_values, counts = np.unique(mask_unit_id[mask_unit_id >= 0],
+                                                    return_counts=True)
+                # Create a mask of the weights
+                weights_mask = np.zeros(data_idx.shape)
+                data_idx_values = np.unravel_index(data_idx_values, data_idx.shape)
+                weights_mask[data_idx_values] = counts / np.sum(counts)
+
+                assert np.isclose(np.sum(weights_mask), 1)
+
+                # Add the mask to the list
+                unit_weights.append(weights_mask)
+
+            block_size = 1000
+            n_steps = 1 + np.ceil(len(self.time) / block_size).astype(int)
+
             with ThreadPoolExecutor(max_workers=num_threads) as executor:
                 # Submit the tasks for each time step to the executor
-                futures = [executor.submit(self._extract_time_step_data, data_var,
-                                           unit_id_masks, unit_ids, unit_ids_nb, t)
-                           for t in range(len(self.time))]
+                futures = [executor.submit(self._extract_time_step_data_weights,
+                                           data_var, unit_weights, t_block, block_size)
+                           for t_block in range(n_steps)]
 
                 # Wait for all tasks to complete
                 concurrent.futures.wait(futures)
@@ -203,7 +249,8 @@ class TimeSeries2D(TimeSeries):
         elapsed_time = time.time() - start_time
         print(f"Elapsed time: {elapsed_time:.2f} seconds (using {num_threads} threads)")
 
-    def _extract_time_step_data(self, data_var, unit_id_masks, unit_ids, unit_ids_nb, t):
+    def _extract_time_step_data_reproject(self, data_var, unit_id_masks, unit_ids,
+                                          unit_ids_nb, t):
         # Print message very 20 time steps
         if t % 20 == 0:
             print(f"Extracting {self.time[t]}")
@@ -216,8 +263,24 @@ class TimeSeries2D(TimeSeries):
         for u in range(unit_ids_nb):
             # Mask the meteorological data with the hydro unit.
             val = hb.xr.where(unit_id_masks[u], data_var_t, np.nan).to_numpy()
-            # Mean the meteorological data in the unit.
-            self.data[-1][t][u] = np.nanmean(val)
+            # Average the meteorological data in the unit.
+            self.data[-1][t, u] = np.nanmean(val)
+
+    def _extract_time_step_data_weights(self, data_var, unit_weights, i_block,
+                                        block_size):
+        i_start = i_block * block_size
+        i_end = min((i_block + 1) * block_size, len(self.time))
+        if i_start >= len(self.time):
+            return
+
+        print(f"Extracting {self.time[i_start]}")
+
+        # Extract data for each unit
+        for u, unit_weight in enumerate(unit_weights):
+            # Mask the meteorological data with the unit weights.
+            self.data[-1][i_start:i_end, u] = np.nansum(
+                data_var[i_start:i_end].to_numpy() * unit_weight,
+                axis=(1, 2))
 
     @staticmethod
     def _parse_crs(data, file_crs):

3c1b05fd4e9d68d5ea0292806108f89691075b66
Pascal Horton <pascal.horton@unibe.ch>
2024-01-30 19:23:14 +0100

    Minor changes to the bootstrapping.

--- python/src/hydrobricks/observations.py
+++ python/src/hydrobricks/observations.py
@@ -18,7 +18,7 @@ class Observations(TimeSeries1D):
 
         Parameters
         ----------
-        metric
+        metric : str
             The abbreviation of the function as defined in HydroErr
             (https://hydroerr.readthedocs.io/en/stable/list_of_metrics.html)
             Examples: nse, kge_2012, ...
@@ -34,6 +34,7 @@ class Observations(TimeSeries1D):
         The value of the selected metric.
         """
         eval_fct = getattr(importlib.import_module('HydroErr'), metric)
+
         return eval_fct(simulations, observations)
 
     def compute_reference_metric(self, metric):
@@ -45,7 +46,7 @@ class Observations(TimeSeries1D):
 
         Parameters
         ----------
-        metric
+        metric: str
             The abbreviation of the function as defined in HydroErr
             (https://hydroerr.readthedocs.io/en/stable/list_of_metrics.html)
             Examples: nse, kge_2012, ...
@@ -54,14 +55,13 @@ class Observations(TimeSeries1D):
         -------
         The mean value of 100 realization of the selected metric.
         """
-
         df = self.time.to_frame().copy()
-        df['Data'] = self.data[0]
-        df['Year'] = pd.DatetimeIndex(df['Date']).year
-        df = df.set_index('Date')
+        df['data'] = self.data[0]
+        df['year'] = pd.DatetimeIndex(df['date']).year
+        df = df.set_index('date')
         # Get rid of the 29 February to always have years of 365 days
         df = df[df.index.strftime('%m-%d') != '02-29']
-        years = df.Year.unique()
+        years = df.year.unique()
 
         df = df.set_index('Year')
 
@@ -69,8 +69,9 @@ class Observations(TimeSeries1D):
         for i in range(100):
             sampled_years = np.random.choice(years, size=years.size, replace=True)
             new_df = df.loc[sampled_years].copy()
-            value = self._eval_metric(metric, new_df.Data.values, df.Data.values)
+            value = self._eval_metric(metric, new_df.data.values, df.data.values)
             metrics[i] = value
 
         ref_metric = np.mean(metrics)
+
         return ref_metric

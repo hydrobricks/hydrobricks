13917791b2164d5f50a4989b5f897b1b65b27c1f
Anne-Laure Argentin <anne.laure.argentin@free.fr>
2025-06-10 01:11:31 +0200

    Fix plot bug

--- python/examples/preprocessing/compute_glacier_evolution_delta_h_from_ice_data.py
+++ python/examples/preprocessing/compute_glacier_evolution_delta_h_from_ice_data.py
@@ -57,9 +57,9 @@ print(f"Files saved to: {working_dir}")
 
 # Load the results from the CSV files
 areas_evol = pd.read_csv(
-    working_dir / "details_glacier_areas_evolution.csv", index_col=0, header=[0, 1, 2])
+    working_dir / "details_glacier_areas_evolution.csv", header=[0, 1, 2])
 we_evol = pd.read_csv(
-    working_dir / "details_glacier_we_evolution.csv", index_col=0, header=[0, 1, 2])
+    working_dir / "details_glacier_we_evolution.csv", header=[0, 1, 2])
 init_glacier_df = pd.read_csv(working_dir / "glacier_profile.csv")
 init_glacier_df = init_glacier_df.drop(
     init_glacier_df[init_glacier_df["('glacier_area', 'm2')"] == 0].index)

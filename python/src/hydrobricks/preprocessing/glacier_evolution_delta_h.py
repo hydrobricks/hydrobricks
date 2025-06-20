3962f48b2850bfcba46d1e9840c70fa81d4a71e7
Anne-Laure Argentin <anne.laure.argentin@free.fr>
2025-06-20 09:45:47 +0200

    Fixing the initial thickness

--- python/src/hydrobricks/preprocessing/glacier_evolution_delta_h.py
+++ python/src/hydrobricks/preprocessing/glacier_evolution_delta_h.py
@@ -170,9 +170,9 @@ class GlacierEvolutionDeltaH:
                 mask_band = map_bands_ids == band_id
                 mask_unit = catchment.map_unit_ids == unit_id
                 masked_thickness = ice_thickness[mask_band & mask_unit]
+                masked_thickness = masked_thickness[masked_thickness > 0]
                 if area_from_topo:
                     self.ice_thicknesses[0][band_id] = masked_thickness * ICE_WE * 1000
-                masked_thickness = masked_thickness[masked_thickness > 0]
 
                 # Compute the mean thickness
                 if masked_thickness.size > 0 and not np.isnan(masked_thickness).all():

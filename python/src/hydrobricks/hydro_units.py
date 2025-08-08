3c4b28569ee8974d4ae61f3eac8185dceda77ee6
Pascal Horton <pascal.horton@unibe.ch>
2025-08-08 16:07:42 +0200

    Fixing tests.

--- python/src/hydrobricks/hydro_units.py
+++ python/src/hydrobricks/hydro_units.py
@@ -259,7 +259,7 @@ class HydroUnits:
         (e.g. ground), set them to 1.
         """
         if len(self.land_cover_names) == 1:
-            self.hydro_units[self.prefix_fraction + self.land_cover_names[0]] = 1
+            self.hydro_units[self.prefix_fraction + self.land_cover_names[0]] = 1.0
             return
 
         for cover_name in self.land_cover_names:
@@ -271,8 +271,8 @@ class HydroUnits:
         # Set the land cover fractions of 'ground' to 1 and the rest to 0
         for cover_name in self.land_cover_names:
             field_name = self.prefix_fraction + cover_name
-            self.hydro_units[(field_name, 'fraction')] = 0
-        self.hydro_units[(self.prefix_fraction + 'ground', 'fraction')] = 1
+            self.hydro_units[(field_name, 'fraction')] = 0.0
+        self.hydro_units[(self.prefix_fraction + 'ground', 'fraction')] = 1.0
 
     def initialize_from_land_cover_change(
             self,

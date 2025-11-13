e01d46d18b9af18e82edca099108ec4b15eb7a61
Pascal Horton <pascal.horton@unibe.ch>
2025-11-13 15:41:58 +0100

    Adding helpers to the Results class.
    Extraction of values across hydro units and the mean snow water equivalent (SWE).

--- python/src/hydrobricks/results.py
+++ python/src/hydrobricks/results.py
@@ -42,6 +42,25 @@ class Results:
             for label in self.labels_aggregated:
                 print('- ' + label)
 
+    def get_land_cover_areas(self, land_cover: str) -> np.ndarray:
+        """
+        Get the areas of a land cover across the hydro units.
+
+        Parameters
+        ----------
+        land_cover
+            The name of the land cover.
+
+        Returns
+        -------
+        The areas of the land cover across the hydro units.
+        """
+        i_land_cover = self.labels_land_cover.index(land_cover)
+        lc_fraction = self.results.land_cover_fractions[i_land_cover, :, :]
+        hydro_units_areas = self.results.hydro_units_areas
+
+        return hydro_units_areas * lc_fraction
+
     def get_hydro_units_values(
             self,
             component: str,
@@ -75,6 +94,78 @@ class Results:
 
         return self.results.hydro_units_values[i_component].sel(
             time=slice(start_date, end_date)).to_numpy()
+
+    def get_mean_hydro_units_values(
+            self,
+            land_cover: str,
+            component: str,
+            start_date: str = None,
+            end_date: str = None
+    ) -> np.ndarray:
+        """
+        Get the mean values of a component across the hydro units.
+
+        Parameters
+        ----------
+        land_cover
+            The name of the land cover.
+        component
+            The name of the component.
+        start_date
+            The start date of the period to extract.
+        end_date
+            The end date of the period to extract (default: None).
+
+        Returns
+        -------
+        The mean values of the component across the hydro units.
+        """
+        values = self.get_hydro_units_values(component, start_date, end_date)
+        lc_areas = self.get_land_cover_areas(land_cover)
+
+        return values * lc_areas / lc_areas.sum(axis=0)
+
+    def get_mean_swe(
+            self,
+            start_date: str = None,
+            end_date: str = None
+    ) -> np.ndarray:
+        """
+        Get the mean snow water equivalent (SWE) across the hydro units.
+
+        Parameters
+        ----------
+        start_date
+            The start date of the period to extract.
+        end_date
+            The end date of the period to extract (default: None).
+
+        Returns
+        -------
+        The mean SWE across the hydro units.
+        """
+        lc_swe = []
+        lc_areas = []
+        for land_cover in self.labels_land_cover:
+            swe = self.get_hydro_units_values(
+                component=f'{land_cover}_snowpack:snow_content',
+                start_date=start_date,
+                end_date=end_date
+            )
+            lc_swe.append(swe)
+            lc_areas.append(self.get_land_cover_areas(land_cover))
+
+        lc_swe = np.array(lc_swe)
+        lc_areas = np.array(lc_areas)
+
+        # Flatten the first dimension (land covers) into the hydro units dimension
+        lc_swe = lc_swe.reshape(-1, lc_swe.shape[2])
+        lc_areas = lc_areas.reshape(-1, lc_areas.shape[2])
+
+        total_areas = lc_areas.sum(axis=0)
+        mean_swe = (lc_swe * lc_areas).sum(axis=0) / total_areas
+
+        return mean_swe
 
     def get_time_array(self, start_date: str, end_date: str) -> np.ndarray:
         """
